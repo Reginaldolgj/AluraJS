d133d107198b4817165ab05e311d28dd4e503b31
Reginaldo <reginaldolgj@gmail.com>
2023-04-06 19:10:51 -0300

    exemplos aula 1

---
+++ JS/Primeiro.js
@@ -0,0 +1,19 @@
+//Tipo number
+
+const meuNumero = 3; //não muda
+
+const checkbox = '\u2705' ;
+
+console.log(checkbox)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+console.log(meuNumero === checkbox);
