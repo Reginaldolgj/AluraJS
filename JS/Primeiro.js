c0d412046227d2387e5d912d50d74514c37023ee
Reginaldo <reginaldolgj@gmail.com>
2023-04-06 20:55:35 -0300

    Alura JS

--- JS/Primeiro.js
+++ JS/Primeiro.js
@@ -1,19 +1,25 @@
-//Tipo number
+// //Tipo number
 
-const meuNumero = 3; //não muda
+// const meuNumero = 3; //não muda
 
-const checkbox = '\u2705' ;
+// const checkbox = '\u2705' ;
 
-console.log(checkbox)
+// console.log(checkbox)
 
-// const cifrao = '\u0024'
-// const aMaiusculo = '\u0041'
-// const tique = '\u2705'
-// const hiragana = '\u3041'
+// // const cifrao = '\u0024'
+// // const aMaiusculo = '\u0041'
+// // const tique = '\u2705'
+// // const hiragana = '\u3041'
 
-console.log(cifrao)
-console.log(aMaiusculo)
-console.log(tique)
-console.log(hiragana)
+// console.log(cifrao)
+// console.log(aMaiusculo)
+// console.log(tique)
+// console.log(hiragana)
 
-console.log(meuNumero === checkbox);
+// console.log(meuNumero === checkbox);
+
+
+let usuarioConectado = false;
+console.log(Number(usuarioConectado)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+usuarioConectado = true;
+console.log(Number(usuarioConectado)); // agora teremos a conversão de true (verdadeiro) para o número 1.
