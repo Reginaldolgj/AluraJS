c1694ca8c0710ef801ba67748d5f8cd1a8e7730e
Reginaldo <reginaldolgj@gmail.com>
2023-04-07 15:59:47 -0300

    Atualização

--- JS/Primeiro.js
+++ JS/Primeiro.js
@@ -19,7 +19,23 @@
 // console.log(meuNumero === checkbox);
 
 
-let usuarioConectado = false;
-console.log(Number(usuarioConectado)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
-usuarioConectado = true;
-console.log(Number(usuarioConectado)); // agora teremos a conversão de true (verdadeiro) para o número 1.
+// let usuarioConectado = false;
+// console.log(Number(usuarioConectado)); // teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+// usuarioConectado = true;
+// console.log(Number(usuarioConectado)); // agora teremos a conversão de true (verdadeiro) para o número 1.
+// console.log("deu erro");
+// console.error("deu erro");
+
+// console.log("deu erro");
+// console.error(new Error("deu erro"));
+
+// let idadeCliente = 19;
+// const idadeMinima = 18;
+// console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco") //ternário 
+
+const nome = "Reginaldo";
+const idade = 2023 - 1996;
+const cidadeNasc = "São Paulo";
+
+const eu = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNasc}`;
+console.log(eu);
