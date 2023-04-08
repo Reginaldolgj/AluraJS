e13f241e76f090b61e9e0531291a7b7adbf060c3
Reginaldo <reginaldolgj@gmail.com>
2023-04-08 11:07:45 -0300

    Arrays

---
+++ JS 1 Arrays/peixes.js
@@ -0,0 +1,14 @@
+// animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+
+// animaisDoAquario.splice(3,2,'🐟')
+
+// console.log(animaisDoAquario)
+
+const nomes = ["Ana", "Juliana", "Leonardo"];
+const idades = [30, 35, 28];
+const faculdade = [false, true, true];
+
+const funcionarios = [nomes, idades, faculdade];
+
+console.log(funcionarios[2][2])
